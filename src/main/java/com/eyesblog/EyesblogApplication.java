0143f48cf2bfbb87b3cf644cb906691f7a69a1a3
eyes <792734338@qq.com>
2022-02-23 21:22:04 +0800

    init

---
+++ src/main/java/com/eyesblog/EyesblogApplication.java
@@ -0,0 +1,13 @@
+package com.eyesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyesblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyesblogApplication.class, args);
+    }
+
+}
